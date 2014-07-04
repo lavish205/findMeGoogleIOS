468a7d176e9e43654d50502cb8c8729dcbbb5916
Lavish <lucky.lavish@gmail.com>
2014-07-04 13:34:51 +0530

    google map working great with customized markers

--- FindMeGoogle/FindMeGoogle/ECSMapView.h
+++ FindMeGoogle/FindMeGoogle/ECSMapView.h
@@ -2,7 +2,7 @@
 //  ECSMapView.h
 //  FindMeGoogle
 //
-//  Created by Developer on 03/07/14.
+//  Created by Developer on 04/07/14.
 //  Copyright (c) 2014 Lavish Aggarwal. All rights reserved.
 //
 
--- FindMeGoogle/FindMeGoogle/ECSMapView.m
+++ FindMeGoogle/FindMeGoogle/ECSMapView.m
@@ -2,7 +2,7 @@
 //  ECSMapView.m
 //  FindMeGoogle
 //
-//  Created by Developer on 03/07/14.
+//  Created by Developer on 04/07/14.
 //  Copyright (c) 2014 Lavish Aggarwal. All rights reserved.
 //
 
@@ -11,6 +11,7 @@
 @interface ECSMapView ()
 {
     GMSMapView *mapView;
+    GMSPanoramaView *panoView;
 }
 @end
 
@@ -29,12 +30,58 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-   
-    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86 longitude:151.20 zoom:6];
-    //mapView = [GMSMapView mapWithFrame:CGRectMake(0, 0, 300, 400)camera:camera];
-    mapView.myLocationEnabled = YES;
-    [self.view addSubview:mapView];
+    //setting camera for viewing the map
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:28.617401	 longitude:77.381254 zoom:6];
     
+    //adding camera to mapview
+    mapView = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+    
+    //enabling user location
+    mapView.myLocationEnabled = YES;
+    
+    //defining map type
+    mapView.mapType = kGMSTypeHybrid;
+    
+    //enabling accessibility elements
+    mapView.accessibilityElementsHidden =NO;
+    
+    //adding compass button on map
+    mapView.settings.compassButton =YES;
+    
+    //addding myLocation Button on map
+    mapView.settings.myLocationButton =YES;
+    
+    //adding map to application view
+    self.view = mapView;
+    
+    
+    //adding marker to map
+    GMSMarker *marker = [[GMSMarker alloc]init];
+    marker.position = CLLocationCoordinate2DMake(28.617401, 77.381254);
+    marker.title = @"Here I am";
+    marker.snippet = @"Your current location";
+    NSLog(@"User Location %@",mapView.myLocation);
+    marker.flat = YES;
+    marker.appearAnimation = kGMSMarkerAnimationPop;
+    marker.map = mapView;
+   
+    
+    GMSMarker *marker2 = [[GMSMarker alloc]init];
+    marker2.position = CLLocationCoordinate2DMake(28.817401, 77.881254);
+    marker2.title = @"Here I am";
+    marker2.snippet = @"Your current location";
+    NSLog(@"User Location %@",mapView.myLocation);
+    marker2.map = mapView;
+    
+    
+    //adding a polyline between two markers
+    GMSMutablePath *path = [GMSMutablePath path];
+    [path addCoordinate:CLLocationCoordinate2DMake(28.617401, 77.381254)];
+    [path addCoordinate:CLLocationCoordinate2DMake(28.817401, 77.881254)];
+    GMSPolyline *polyline = [GMSPolyline polylineWithPath:path];
+   
+    polyline.map = mapView;        
+
 }
 
 - (void)didReceiveMemoryWarning
